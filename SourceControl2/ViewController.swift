6e8bd1d4542874a94b957fb4ab0e24fea2cfd67f
rocketalbert <rocketalbert114@gmail.com>
2019-06-15 10:49:32 -0600

    Initial Commit

---
+++ SourceControl2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SourceControl2
+//
+//  Created by Albert Yu on 6/15/19.
+//  Copyright © 2019 AlbertLLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
